74772d7a7f4c9784cabc20780bd6e0fec2bed1f8
League Student <league@WTS14-4.attlocal.net>
2019-06-03 19:50:21 -0700

    trying again

--- limiter.sh
+++ limiter.sh
@@ -1,6 +1,16 @@
     
 #!/bin/bash  
 
+TRANSGRESSIONS=0
+
+while true
+do
 if [ $(osascript -e 'output volume of (get volume settings)') -gt 25 ] ; then
-osascript -e 'set volume output volume 2';
-fi;
+osascript -e 'set volume output volume 2'
+((TRANSGRESSIONS++))
+fi
+if [ $TRANSGRESSIONS -ge 3 ]; then
+osascript -e 'set volume output volume 0'
+echo "locked"
+fi
+done
